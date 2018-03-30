721ff6b944fbece86124e88685b9c170cbd9bb0e
olikishor <olikishor@outlook.com>
2018-03-30 18:06:18 +0545

    upto day 4

---
+++ MVC_web/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using MVC_web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        // GET: Authentication
+        [Authorize]
+        public ActionResult Login()
+        {
+            EmployeeListViewModel employeeListViewModel = new EmployeeListViewModel();
+            return View();
+        }
+    }
+}
